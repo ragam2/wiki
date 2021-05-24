c45ba161f881b424c7d11190d31efa08bbd93dbf
Rahman Gamble <thegamble@google.com>
2021-05-24 19:17:31 +0000

    add test for home page

--- wiki_test.py
+++ wiki_test.py
@@ -1,5 +1,21 @@
+import pytest
+
 import wiki
+
+
+@pytest.fixture
+def client():
+    wiki.app.config["TESTING"] = True
+
+    with wiki.app.test_client() as client:
+        yield client
 
 
 def test_import():
     assert wiki is not None
+
+
+def test_homepage(client):
+    resp = client.get("/")
+    assert resp.status_code == 200
+    assert b"Hello, World!\n" in resp.data
