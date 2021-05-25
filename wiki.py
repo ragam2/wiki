68c1f6be503a03370fea4d7e78f3c1623517be95
jeanrivera <jeanrivera@google.com>
2021-05-25 19:59:06 +0000

    Added templates and pages directories

--- wiki.py
+++ wiki.py
@@ -1,7 +1,35 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return "Hello, World!\n"
+
+@app.route(...)
+def handle_request(...):
+    # TODO: load the desired page content
+    return render_template(
+        "main",
+        page_name=...,
+        page_content=...,
+    )
+
+@app.route("/view/<pagename>")
+def get_frontpage():
+    # TODO: load the desired page content
+    return render_template(
+        "main",
+        page_name=...,
+        page_content=...,
+    )
+
+@app.route("/view/<pagename>")
+def get_pagename(pagename):
+    # TODO: load the desired page content
+    return render_template(
+        "main",
+        page_name=...,
+        page_content=...,
+    )
