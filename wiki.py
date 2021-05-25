1dacc67ea774cd5d02e762c68037d5268493bd49
Rahman Gamble <thegamble@google.com>
2021-05-25 21:05:54 +0000

    Added get_frontpage and get_pagename to wiki.py, and tests

--- wiki.py
+++ wiki.py
@@ -16,3 +16,25 @@ def main():
         page_name=pn,
         page_content=contents,
     )
+
+@app.route("/view/")
+def get_frontpage():
+    with open("pages/FrontPage.txt", "r") as f:
+        contents = f.read()
+    return render_template(
+        "main.html",
+        page_name="Front Page",
+        page_content=contents,
+    )
+
+@app.route("/view/<pagename>")
+def get_pagename(pagename):
+    fullpath = "pages/" + pagename + ".txt"
+    with open(fullpath, "r") as f:
+        pn = f.read()
+    
+    return render_template(
+        "main.html",
+        page_name=pagename,
+        page_content=pn,
+    )
